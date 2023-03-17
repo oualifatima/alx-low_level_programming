0b2a9cb847fef06981369687fcfed4a04f6451a2
ouali fatima <oualifatima082@gmail.com>
2023-03-16 19:55:14 -0700

    l

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-
 /**
- *main - this is the main function
- *Return: 0 when successful
+ * main - this is the main function
+ * Return: 0 when successful
  */
-
 int main(void) 
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
