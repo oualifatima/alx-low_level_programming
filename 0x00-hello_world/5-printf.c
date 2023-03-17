47b6057df8de6a32f9f7de7a2abd204da8696135
ouali fatima <oualifatima082@gmail.com>
2023-03-16 19:46:47 -0700

    kfdjhsjd

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,11 @@
 
 /**
  *main - this is the main function
- *
  *Return: 0 when successful
  */
 
 int main(void) 
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }	
